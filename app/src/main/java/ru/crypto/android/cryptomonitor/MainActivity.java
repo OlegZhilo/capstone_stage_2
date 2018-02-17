f2dff3dca7aa881ffe4cf724cd1721afd4d7f69b
OlegZhilo <oleg.zhilo@surfstudio.ru>
2018-02-17 23:59:27 +0300

    Added base dependencies

--- app/src/main/java/ru/crypto/android/cryptomonitor/MainActivity.java
+++ app/src/main/java/ru/crypto/android/cryptomonitor/MainActivity.java
@@ -1,13 +1,15 @@
 package ru.crypto.android.cryptomonitor;
 
+import android.arch.lifecycle.LifecycleOwner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LifecycleOwner{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
